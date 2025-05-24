670786d909b1a5a20865d843715ba3cd630d5d95
Wambaborel <wambadata123@gmail.com>
2025-05-24 21:11:46 +0200

    feat: Enhance Library Management system with Article and Library Member functionalities, including new templates and validation logic for transactions

--- library_management/library_management/doctype/library_member/library_member.js
+++ library_management/library_management/doctype/library_member/library_member.js
@@ -1,8 +1,26 @@
+/**
+ * Frappe client-side script for the 'Library Member' doctype.
+ * 
+ * Adds custom buttons to the form for creating related 'Library Membership' and 'Library Transaction' documents,
+ * pre-filling the 'library_member' field with the current member's name.
+ * 
+ * @namespace frappe.ui.form.on
+ * @param {frappe.ui.Form} frm - The current form instance for 'Library Member'.
+ */
 // Copyright (c) 2025, w data and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Library Member", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('Library Member', {
+ refresh: function(frm) {
+ frm.add_custom_button('Create Membership', () => {
+ frappe.new_doc('Library Membership', {
+ library_member: frm.doc.name
+ })
+ })
+ frm.add_custom_button('Create Transaction', () => {
+ frappe.new_doc('Library Transaction', {
+ library_member: frm.doc.name
+ })
+ })
+ }
+});
---
+++ library_management/library_management/doctype/library_settings/test_library_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, w data and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLibrarySettings(FrappeTestCase):
+	pass
